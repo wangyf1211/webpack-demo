0c86e502de7a827390c5d4207be1f796fcdf5648
wyf <wyf@wyf-MacBook-Pro.local>
2019-03-15 20:37:10 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
-
-  var myIcon=new Image();
-  myIcon.src=Icon;
-
-  element.appendChild(myIcon);
-
-  var ele=document.createElement('div')
-  ele.style.width='100px'
-
-  element.appendChild(ele);
-  
-  console.log(Data)
-
   return element;
 }
 
