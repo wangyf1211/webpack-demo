7b477d9cc5c3e8559b60ff6b1055ba517b57c8e1
wyf <wyf@wyf-MacBook-Pro.local>
2019-03-16 15:33:55 +0800

    install webpack-merge

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
-import _ from 'lodash'
+import {cube} from './math.js'
 import printMe from './print.js'
 import './style.css'
 
 function component() {
-  var element = document.createElement('div');
-  var btn=document.createElement('button');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML='click me and check the console';
-  btn.style.backgroundColor='#ddd';
-  btn.onclick=printMe;
-  element.appendChild(btn);
+  var element = document.createElement('pre');
+  element.innerHTML=[
+    'hello webpack!',
+    '5 cubed is equal to '+cube(5)
+  ].join('\n\n')
+  
   return element;
 }
 let element=component()
 document.body.appendChild(element);
-
-if(module.hot){
-  module.hot.accept('./print.js',()=>{
-    console.log('Accepting the updated printMe module');
-    document.body.removeChild(element)
-    element=component()
-    document.body.appendChild(element)
-  })
-}
